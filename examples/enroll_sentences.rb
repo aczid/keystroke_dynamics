84a083d05f6b8f921e3dfb706d41af425728accf
aczid <aczid@maggie.(none)>
2009-01-21 21:06:08 +0100

    Fixed several occurences of the same spelling error

--- examples/enroll_sentences.rb
+++ examples/enroll_sentences.rb
@@ -34,7 +34,7 @@ loginbox.pack_start(pass, true, true, 0)
 mainbox.pack_start(loginbox, true, true,0)
 mainbox.show
 
-mainbox.pack_start(Gtk::Label.new("Please type these scentences so I can learn your keyboard dynamics."), true, true, 20)
+mainbox.pack_start(Gtk::Label.new("Please type these scentences so I can learn your keystroke dynamics."), true, true, 20)
 
 scentences = [
   Gtk::Label.new("Two driven jocks help fax my big quiz."),
